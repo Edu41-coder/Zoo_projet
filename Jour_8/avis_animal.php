47ab0362b4e02d82e963e890ea7e5b7d8458df62
Edu41-coder <153880703+Edu41-coder@users.noreply.github.com>
2024-05-12 09:35:39 +0200

    Add files via upload

---
+++ Jour_8/avis_animal.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Avis du vétérinaire</title>
+    <!-- Bootstrap 5.1 CSS -->
+    <link href="styles/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="styles/zoo.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <?php
+        require 'verification_connexion.php';
+        $page = 'habitat';
+        include("menu.php");
+        if ($role != "admin") {
+            header('Location: liste_habitat.php');
+        }
+        $id = $_GET['id'];
+        $requete = 'SELECT *
+                FROM rapport_veterinaire
+                WHERE rapport_veterinaire.animal_id = ' . $_GET['id'];
+        $exe = $bdd->query($requete);
+        $resultats = $exe->fetchAll(PDO::FETCH_ASSOC);
+        setlocale(LC_TIME, 'fr_FR.UTF-8');
+    ?>
+    <div class="container">
+        <h1>Avis du vétérinaire pour l'animal </h1>
+
+        <div class="divDebut">
+            <button type="button" class="btn btn-success btn-lg"><a class="lien" href="liste_habitat.php">Retour à la liste des habitats</a></button>
+        </div>
+        <?php
+            if (count($resultats) == 0) {
+                ?>
+                    <p class="mt10perso">Aucun avis du vétérinaire pour cet animal.</p>
+                <?php
+            }
+            foreach ($resultats as $ligne) {
+                $dateFormatee = strftime('%e %B %Y', strtotime($ligne['date']));
+                ?>
+                    <h2 class="mt30perso">Avis du vétérinaire du <?php echo $dateFormatee; ?></H2>
+                    <p class="mt10perso"><span class="boldSpan">État : </span><?php echo $ligne['etatAvis'] ?></p>
+                    <p><span class="boldSpan">Détail de l'état : </span><?php echo $ligne['detail'] ?></p>
+                <?php
+            }
+        ?>
+    </div>
+    <?php
+        include("footer.php");
+    ?>
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="./js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
