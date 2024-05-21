c27469c1bdaec20f56bf5dee11325714ac41ea16
Edu41-coder <153880703+Edu41-coder@users.noreply.github.com>
2024-05-21 05:23:29 +0200

    Delete suppr_service.php

--- suppr_service.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $id = ($_GET['id']);
-
-    include('config.inc.php');
-    $bdd = new PDO('mysql:host='.BDD_SERVER.';dbname='.BDD_DATABASE.';charset=utf8', BDD_LOGIN, BDD_PASSWORD);
-    $requete = 'DELETE FROM service WHERE service_id = ' .$id;
-    $bdd->query($requete); 
-    header('Location: liste_service.php?message=supprOk');
